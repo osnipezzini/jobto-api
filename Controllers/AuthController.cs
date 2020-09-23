51f1fb3baa0ed9f1dfc38f80da9f1243bb9e9127
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-09-23 04:18:09 -0300

    Added JWT Authentication

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using JobTo.API.Data;
+using JobTo.Common.Helpers;
+using JobTo.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly JobToContext _context;
+
+        public AuthController(JobToContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate(
+            [FromBody]User model)
+        {
+            var password = Crypt.Encrypt(model.Password);
+            var user = await _context.Users.Where(
+                x => x.Username == model.Username && x.Password == password)
+                .FirstOrDefaultAsync();
+            if (user == null)
+            {
+                return NotFound(new User());
+            }
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            user.Token = token;
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<dynamic>> RegisterUser(
+            [FromBody]User model)
+        {
+            var modelClean = model;
+            modelClean.Password = Crypt.Encrypt(model.Password);
+            model.Role = "User";
+            
+            var user = _context.Users.Add(modelClean);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProfile", new { id = modelClean.Id }, user);
+        }
+
+        [HttpGet("{id}")]
+        [Route("profile/{id}")]
+        public async Task<ActionResult<User>> GetProfile(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.Password = string.Empty;
+            return Ok(user);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,16 @@
+using System;
+using System.Security.Claims;
+using System.Text;
 using JobTo.API.Data;
+using JobTo.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JobTo.API
 {
@@ -23,14 +28,38 @@ namespace JobTo.API
         {
             services.AddDbContext<JobToContext>(
                 context => context.UseNpgsql(
-                    Configuration.GetConnectionString("Default"), 
+                    Configuration.GetConnectionString("Default"),
                     options => options.SetPostgresVersion(new Version(9, 6)))
             );
-
+            services.AddCors();
             services.AddControllers()
                 .AddNewtonsoftJson(
                         opt => opt.SerializerSettings.ReferenceLoopHandling =
                             Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            services.AddAuthorization(
+                o => o.AddPolicy("apiPolicy", 
+                b => b.RequireAuthenticatedUser()
+                .RequireClaim(ClaimTypes.Role, "User", "Staff")
+                )
+            );
             services.AddScoped<IRepository, DatabaseRepository>();
         }
 
@@ -40,13 +69,17 @@ namespace JobTo.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+                app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
             }
-            
+
             //app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
